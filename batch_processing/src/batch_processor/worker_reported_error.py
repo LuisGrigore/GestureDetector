7ebe174c21188c294fd103242dea32f3ca7a6fde
LuisGrigore <LuisGrigore@users.noreply.github.com>
2025-12-29 15:18:48 +0100

    Chackpoint

--- batch_processing/src/batch_processor/worker_reported_error.py
+++
@@ -1,4 +0,0 @@
-class WorkerReportedError(Exception):
-    def __init__(self, info):
-        super().__init__(f"Worker reported exception: {info.message}")
-        self.info = info
--- batch_processing/src/context.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Event
-from typing import Optional
-
-
-class ControlContext:
-    def __init__(self):
-        self.stop_event = Event()
-        self.abort_event = Event()
-        self.fatal_exception: Optional[Exception] = None
